d3218bce24bded8aa831636fb6fe676a2276328f
Nai Williams <naiwilliams97@gmail.com>
2022-05-05 02:05:33 -0400

    scss modifications

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let apiKey = '9b9bc44cc70f3fd327b277e80a6e788b';
 
 //Toggle sidebar
 document.addEventListener('click', (e) => {
-  if (e.target == document.querySelector('.menu')) {
-    document.querySelector('.sidebar-nav').classList.toggle('hide');
+  if (e.target == document.querySelector('.nav__icon--menu')) {
+    document.querySelector('.main__nav').classList.toggle('hide');
   }
 });
 
@@ -33,7 +33,7 @@ switchUnits.addEventListener('change', () => {
 
 //Refresh Data
 document.addEventListener('click', (e) => {
-  if (e.target == document.querySelector('.refresh')) {
+  if (e.target == document.querySelector('.nav__icon--refresh')) {
     getWeather();
   }
 });
@@ -68,7 +68,7 @@ let getCurrentLocation = () => {
 };
 
 document.addEventListener('click', (e) => {
-  if (e.target == document.querySelector('.location')) {
+  if (e.target == document.querySelector('.nav__icon--geolocation')) {
     getCurrentLocation();
   }
 });
@@ -80,7 +80,7 @@ let getCityBrowser = () => {
   )
     .then((res) => res.json())
     .then((city) => {
-      document.querySelector('.normal-fc .city').innerHTML = city.locality;
+      document.querySelector('.current-weather__city').innerHTML = city.locality;
       unitToggle();
       getWeather();
     });
@@ -91,7 +91,7 @@ let getCityGoogle = () => {
     result = autocomplete.getPlace();
     lat = result.geometry.location.lat();
     lon = result.geometry.location.lng();
-    document.querySelector('.normal-fc .city').innerHTML = result.vicinity;
+    document.querySelector('.current-weather__city').innerHTML = result.vicinity;
     unitToggle();
     getWeather();
   });
@@ -123,41 +123,41 @@ let getWeather = () => {
       //Current
       let d = new Date();
       let now = new Date(weather.current.dt * 1000);
-      document.querySelector('.date').innerHTML = `${days[now.getDay()]} - ${
+      document.querySelector('.current-weather__date').innerHTML = `${days[now.getDay()]} - ${
         months[now.getMonth()]
       } ${now.getDate()}, ${now.getFullYear()}`;
-      document.querySelector('.time').innerHTML = d.toLocaleTimeString([], {
+      document.querySelector('.current-weather__time').innerHTML = d.toLocaleTimeString([], {
         timeZone: weather.timezone,
         hour: 'numeric',
         minute: '2-digit',
       });
-      document.querySelector('.normal-fc .temp').innerHTML = `${Math.round(
+      document.querySelector('.current-weather__temp').innerHTML = `${Math.round(
         weather.current.temp
       )}${tempSymbol}`;
-      document.querySelector('.high-low .low').innerHTML = Math.round(weather.daily[0].temp.min);
-      document.querySelector('.high-low .high').innerHTML = Math.round(weather.daily[0].temp.max);
+      document.querySelector('.current-weather__low').innerHTML = Math.round(weather.daily[0].temp.min);
+      document.querySelector('.current-weather__high').innerHTML = Math.round(weather.daily[0].temp.max);
 
       //Extra
-      document.querySelector('.wind .text').innerHTML = `${Math.round(
+      document.querySelector('.current-weather__text--wind').innerHTML = `${Math.round(
         weather.current.wind_speed
       )} ${speedSymbol}`;
       if (weather.daily[0].rain) {
-        document.querySelector('.precip .text').innerHTML = weather.daily[0].rain;
+        document.querySelector('.current-weather__text--rain').innerHTML = weather.daily[0].rain;
       } else {
-        document.querySelector('.precip .text').innerHTML = '0';
+        document.querySelector('.current-weather__text--rain').innerHTML = '0';
       }
-      document.querySelector('.humidity .text').innerHTML = weather.current.humidity;
-      document.querySelector('.uv .text').innerHTML = weather.current.uvi;
+      document.querySelector('.current-weather__text--humidity').innerHTML = weather.current.humidity;
+      document.querySelector('.current-weather__text--uv').innerHTML = weather.current.uvi;
 
       //Hourly
       let sunrise = new Date(weather.current.sunrise * 1000);
       let sunset = new Date(weather.current.sunset * 1000);
-      document.querySelector('.rise-time').innerHTML = sunrise.toLocaleTimeString([], {
+      document.querySelector('.future-weather__rise-time').innerHTML = sunrise.toLocaleTimeString([], {
         timeZone: weather.timezone,
         hour: 'numeric',
         minute: '2-digit',
       });
-      document.querySelector('.set-time').innerHTML = sunset.toLocaleTimeString([], {
+      document.querySelector('.future-weather__set-time').innerHTML = sunset.toLocaleTimeString([], {
         timeZone: weather.timezone,
         hour: 'numeric',
         minute: '2-digit',
@@ -165,38 +165,38 @@ let getWeather = () => {
       let hourlyF = '';
       weather.hourly.forEach((hour) => {
         hourlyF += `
-        <div class="hour">
-          <p class = "time">${new Date(hour.dt * 1000).toLocaleTimeString([], {
+        <div class="hour__time">
+          <p class = "hour__num">${new Date(hour.dt * 1000).toLocaleTimeString([], {
             timeZone: weather.timezone,
             hour: 'numeric',
             hour12: 'true',
           })}</p>
-          <div class="weather-icon"></div>
-          <p class="temp">${Math.round(hour.temp)}</p>
+          <div class="hour__icon"></div>
+          <p class="hour__temp">${Math.round(hour.temp)}</p>
         </div>`;
       });
-      document.querySelector('.hour-fc .chart').innerHTML = hourlyF;
+      document.querySelector('.future-weather__chart--hour').innerHTML = hourlyF;
 
       //Tomorrow
-      document.querySelector('.tomorrow .temp').innerHTML = `${Math.round(
+      document.querySelector('.future-weather__temp--tomorrow').innerHTML = `${Math.round(
         weather.daily[1].temp.day
       )}`;
-      document.querySelector('.tomorrow .condition').innerHTML =
+      document.querySelector('.future-weather__condition').innerHTML =
         weather.daily[1]['weather'][0]['main'];
 
       //7 Day
       let weeklyF = '';
       weather.daily.forEach((day) => {
         weeklyF += `
-        <div class="day">
-          <p class="weekday">${days[new Date(day.dt * 1000).getDay()]}</p>
-          <div class="weather-icon"></div>
-          <div class="high-low">
-          <p class="low temp">${Math.round(day.temp.min)}</p>
-          <p class="high temp">${Math.round(day.temp.max)}</p>
+        <div class="week__day">
+          <p class="week__weekday">${days[new Date(day.dt * 1000).getDay()]}</p>
+          <div class="week__icon"></div>
+          <div class="week__high-low">
+          <p class="week__low">${Math.round(day.temp.min)}</p>
+          <p class="week__high">${Math.round(day.temp.max)}</p>
         </div>`;
       });
-      document.querySelector('.week-fc .chart').innerHTML = weeklyF;
+      document.querySelector('.future-weather__chart--week').innerHTML = weeklyF;
     });
 };
 
